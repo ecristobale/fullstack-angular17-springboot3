e58dde68fe090675ecef364a100f8b52435ae911
ecristobale <edu_ce_1988@hotmail.com>
2025-02-16 00:34:29 +0100

    invoice service that retrieves info and it is showed in invoice component

---
+++ angular-facturacion/angular-invoice-app/src/app/data/invoice.data.ts
@@ -0,0 +1,41 @@
+import { Invoice } from "../models/invoice";
+
+export const invoiceData: Invoice = {
+
+    id: 1,
+    name: 'PC components',
+    client: {
+        name: 'John',
+        lastName: 'Doe',
+        address: {
+            country: 'USA',
+            city: 'Los Angeles',
+            street: 'Elm Street',
+            number: 15
+        }
+    },
+    company: {
+        name: 'Ragnarock',
+        fiscalNumber: 4627326,
+    },
+    items: [
+        {
+            id: 1,
+            product: 'CPU Intel i7',
+            price: 300,
+            quantity: 1
+        },
+        {
+            id: 2,
+            product: 'Msoft Keyboard',
+            price: 10,
+            quantity: 2
+        },
+        {
+            id: 3,
+            product: 'Nokia Monitor',
+            price: 150,
+            quantity: 3
+        }
+    ]
+}
