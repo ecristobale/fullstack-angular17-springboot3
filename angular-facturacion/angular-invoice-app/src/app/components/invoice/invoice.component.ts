dee288771e411396e137f970071134367122c64c
ecristobale <edu_ce_1988@hotmail.com>
2025-02-16 18:48:41 +0100

    Refactor invoice component. Used subcomponents

--- angular-facturacion/angular-invoice-app/src/app/components/invoice/invoice.component.ts
+++ angular-facturacion/angular-invoice-app/src/app/components/invoice/invoice.component.ts
@@ -5,12 +5,11 @@ import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
 import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
-import { RowItemComponent } from '../row-item/row-item.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent, RowItemComponent],
+  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent],
   templateUrl: './invoice.component.html'
 })
 export class InvoiceComponent implements OnInit {
