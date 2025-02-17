18e04668d5c5927aef0587c68ef30e5fd983127d
ecristobale <edu_ce_1988@hotmail.com>
2025-02-17 23:23:33 +0100

    incrementing quantity for repeated products

--- angular-cart/angular-cart-app/src/app/components/cart-app.component.ts
+++ angular-cart/angular-cart-app/src/app/components/cart-app.component.ts
@@ -24,6 +24,22 @@ export class CartAppComponent implements OnInit {
   }
 
   onAddCart(product: Product) {
-    this.items = [... this.items, {product: {... product}, quantity: 1}];
+    // const hasItem = this.items.find(item => {
+    //   return item.product.id === product.id;
+    // });
+    const hasItem = this.items.find(item => item.product.id === product.id);
+    if(hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id) {
+          return {
+            ... item, 
+            quantity: ++item.quantity
+          };
+        }
+        return item;
+      });
+    } else {
+      this.items = [... this.items, {product: {... product}, quantity: 1}];
+    }
   }
 }
