ed3efbc90f356fa8f11bd95364af4a3c9f776dc4
ecristobale <edu_ce_1988@hotmail.com>
2025-02-19 20:51:37 +0100

    configured NgRx (redux) for managing counter actions

--- angular-ngrx/angular-counter-app/src/app/components/counter.component.ts
+++ angular-ngrx/angular-counter-app/src/app/components/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrease, increment, reset } from '../store/items.action';
 
 @Component({
   selector: 'app-counter',
@@ -10,24 +12,30 @@ export class CounterComponent {
 
   title: string = 'Counter by using Redux (NgRx) for managing the state';
 
-  counter!: number;
+  counterComponent!: number;
 
-  constructor() {
-    this.counter = 0;
+  constructor(private store: Store<{count: number}>) {
+    this.counterComponent = 0;
+    this.store.select('count').subscribe(counterStore => {
+      this.counterComponent = counterStore;
+    })
   }
 
   increment(): void {
-    this.counter++;
+    // this.counterComponent++;
+    this.store.dispatch(increment());
     console.log('Incrementing counter value...');
   }
 
   decrease(): void {
-    this.counter--;
+    // this.counterComponent--;
+    this.store.dispatch(decrease());
     console.log('decreasing counter value...');
   }
 
   reset(): void {
-    this.counter = 0;
+    // this.counterComponent = 0;
+    this.store.dispatch(reset());
     console.log('resetting counter value...');
   }
 }
