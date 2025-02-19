6ef22d85ba3113873e895b1e17f532b456032c4c
ecristobale <edu_ce_1988@hotmail.com>
2025-02-19 14:04:52 +0100

    Recover actions of adding elements to shopping cart and calculate total. Passing via angular routes using <a [state] and the constructor of the component of the route

--- angular-cart/angular-cart-app/src/app/components/cart/cart.component.ts
+++ angular-cart/angular-cart-app/src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter} from '@angular/core';
 import { CartItem } from '../../models/cartItem';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cart',
@@ -14,6 +15,11 @@ export class CartComponent {
   total: number = 0;
 
   idProductEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  constructor(private router: Router) {
+    this.items = router.getCurrentNavigation()?.extras.state!['items'];
+    this.total = router.getCurrentNavigation()?.extras.state!['total'];
+  }
 
   onDeleteProductOnCart(productId: number) {
     this.idProductEventEmitter.emit(productId);
