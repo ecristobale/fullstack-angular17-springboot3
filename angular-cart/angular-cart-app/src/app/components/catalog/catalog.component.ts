e632c0b4a1f648d00070956ea7a1ab55bb1e6176
ecristobale <edu_ce_1988@hotmail.com>
2025-02-20 01:32:00 +0100

    used store for retrieving catalog products

--- angular-cart/angular-cart-app/src/app/components/catalog/catalog.component.ts
+++ angular-cart/angular-cart-app/src/app/components/catalog/catalog.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { SharingDataService } from '../../services/sharing-data.service';
 import { ProductService } from '../../services/product.service';
+import { Store } from '@ngrx/store';
+import { load } from '../../store/products.actions';
 
 @Component({
   selector: 'catalog',
@@ -15,11 +17,14 @@ export class CatalogComponent implements OnInit {
   products!: Product[];
 
   constructor(
+    private store: Store<{products: any}>,
     private productService: ProductService,
-    private sharingDataService: SharingDataService) {}
+    private sharingDataService: SharingDataService) {
+      this.store.select('products').subscribe(state => this.products = state.products);
+    }
 
   ngOnInit(): void {
-    this.products = this.productService.findAll();
+    this.store.dispatch(load({products: this.productService.findAll()}));
   }
   
   onAddCart(product: Product) {
