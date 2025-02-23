ef7908b9492a32db4fe5e06c3648b215c93e1b5c
ecristobale <edu_ce_1988@hotmail.com>
2025-02-23 17:47:09 +0100

    configured CORS in order to allow angular app to connect to spring boot api

--- fullstack-CRUD-users-app/angular-user-app/src/app/services/user.service.ts
+++ fullstack-CRUD-users-app/angular-user-app/src/app/services/user.service.ts
@@ -14,8 +14,6 @@ export class UserService {
 
   findAll(): Observable<User[]> {
     // return of(this.users);
-    return this.http.get('http://localhost:8080/api/users').pipe(
-      map((users: any) => users as User[]),
-    );
+    return this.http.get<User[]>('http://localhost:8080/api/users');
   }
 }
--- fullstack-CRUD-users-app/spboot-user-app/users-backend/src/main/java/com/springboot/backend/ecristobale/usersapp/users_backend/controllers/UserController.java
+++ fullstack-CRUD-users-app/spboot-user-app/users-backend/src/main/java/com/springboot/backend/ecristobale/usersapp/users_backend/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.backend.ecristobale.usersapp.users_backend.entities.User;
 import com.springboot.backend.ecristobale.usersapp.users_backend.services.UserService;
 
+// @CrossOrigin(originPatterns = "*")
+@CrossOrigin(origins={"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
