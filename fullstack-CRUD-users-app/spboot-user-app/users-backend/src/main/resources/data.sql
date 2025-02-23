c540c5c5531895257f883fb6eff3acb8e0666371
ecristobale <edu_ce_1988@hotmail.com>
2025-02-23 16:20:56 +0100

    Inserted sample values on users table

---
+++ fullstack-CRUD-users-app/spboot-user-app/users-backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, lastname, email, username, password) 
+VALUES 
+('Eduardo', 'Cristóbal', 'edu_ce_1988@hotmail.com', 'usernameee', 'passwooooord'),
+('Jonh', 'Doe', 'johndoe@gmail.com', 'jonndoeee', '54321678'),
+('Cris', 'Doe', 'crisdoe@gmail.com', 'crissdowww', 'estoesunapwd');
